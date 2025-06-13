9b66b5fc5a6d75b6311c7622a3750494f2cf8869
Mannea11 <manne.ande@gmail.com>
2025-06-13 18:49:26 +0200

    ny controller1

--- src/main/java/Crime/Controller/HomeController.java
+++ src/main/java/Crime/Controller/HomeController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "index";
+        return "redirect:/index.html";
     }
 }
